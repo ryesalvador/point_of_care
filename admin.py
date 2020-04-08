87f9e2f6763f25cd1926498b0a2f26ed6de6d038
Rye Salvador <salvadorrye@gmail.com>
2020-04-08 16:46:31 +0800

    Add model Resident and register to admin site

--- admin.py
+++ admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Resident
 
 # Register your models here.
+# admin.site.register(Resident)
+
+@admin.register(Resident)
+class ResidentAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name')
+    search_fields = ('last_name', 'first_name')
--- models.py
+++ models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Resident(models.Model):
+    first_name = models.CharField(max_length=35, verbose_name='name')
+    last_name = models.CharField(max_length=35)
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+
+    def get_absolute_url(self):
+        return reverse('resident-detail', args=[str(self.id)])
