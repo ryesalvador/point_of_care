5fcc63b84b15e84304a3c789d193a990bf292f68
Rye Salvador <salvadorrye@gmail.com>
2020-04-09 11:42:52 +0800

    Create a home page

--- views.py
+++ views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from point_of_care.models import Resident
 
 # Create your views here.
+def index(request):
+    num_residents = Resident.objects.all().count()
+    context = {
+        'num_residents': num_residents,    
+    }
+    return render(request, 'point_of_care/index.html', context=context)
