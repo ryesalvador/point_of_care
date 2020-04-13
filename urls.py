71edb9a276ccbb30927cc51541b5e685610bf93b
Rye Salvador <salvadorrye@gmail.com>
2020-04-13 17:12:20 +0800

    CRUD model resident

--- urls.py
+++ urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('residents/', views.ResidentListView.as_view(), name='residents'),
     path('resident/<int:pk>', views.ResidentDetailView.as_view(), name='resident-detail'),
+    path('resident/create/', views.ResidentCreate.as_view(), name='resident-create'),
+    path('resident/<int:pk>/update/', views.ResidentUpdate.as_view(), name='resident-update'),
+    path('resident/<int:pk>/delete/', views.ResidentDelete.as_view(), name='resident-delete'),
 ]
 
 urlpatterns += [
--- views.py
+++ views.py
@@ -3,6 +3,7 @@ from point_of_care.models import Resident
 from django.views import generic
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 # Create your views here.
 @login_required
@@ -19,3 +20,16 @@ class ResidentListView(LoginRequiredMixin, generic.ListView):
 
 class ResidentDetailView( LoginRequiredMixin, generic.DetailView):
     model = Resident
+
+class ResidentCreate(LoginRequiredMixin, generic.CreateView):
+    model = Resident
+    fields = '__all__'
+
+class ResidentUpdate(LoginRequiredMixin, generic.UpdateView):
+    model = Resident
+    fields = '__all__'
+    template_name_suffix = '_update_form'
+
+class ResidentDelete(LoginRequiredMixin, generic.DeleteView):
+    model = Resident
+    success_url = reverse_lazy('residents')
